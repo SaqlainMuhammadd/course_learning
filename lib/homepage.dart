fe1d03de787a218f03029fe7da81a202452f181c
muhammadsaqlain143 <miansaqlainqureshi@gmail.com>
2024-04-26 20:25:32 +0500

    Updated

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -16,118 +16,48 @@ class homePage extends StatefulWidget {
 
 class _homePageState extends State<homePage> {
   late PageController _pageController;
+  late int _selectedTab;
 
   @override
+  // int _selectedTab = 0;
   void initState() {
     super.initState();
-    _pageController = PageController(initialPage: 0);
+    _selectedTab = 0; // Initialize here
+    _pageController = PageController(initialPage: _selectedTab);
   }
 
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
+  List _pages = [
+    homePageScreen(),
+    coursesPage(),
+    codeEditor(),
+    profilePage(),
+  ];
+
+  _changeTab(int index) {
+    setState(() {
+      _selectedTab = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        children: const <Widget>[
-          homePageScreen(),
-          coursesPage(),
-          codeEditor(),
-          profilePage(),
-        ],
-      ),
-      extendBody: true,
-      bottomNavigationBar: RollingBottomBar(
-        color: Color.fromARGB(255, 255, 240, 219),
-        controller: _pageController,
-        flat: true,
-        useActiveColorByDefault: false,
+      body: _pages[_selectedTab],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (index) => _changeTab(index),
+        selectedItemColor: Colors.teal,
+        unselectedItemColor: Colors.grey,
         items: [
-          RollingBottomBarItem(
-            Icons.home,
-            label: 'Home',
-            activeColor: Colors.redAccent,
-          ),
-          RollingBottomBarItem(
-            Icons.camera,
-            label: 'Camera',
-            activeColor: Colors.blueAccent,
-          ),
-          RollingBottomBarItem(
-            Icons.person,
-            label: 'Person',
-            activeColor: Colors.green,
-          ),
-          RollingBottomBarItem(
-            Icons.person,
-            label: 'Person',
-            activeColor: Colors.green,
-          )
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book_outlined), label: "Courses"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.code_off_outlined), label: "Code Editor"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_3_outlined), label: "Profile"),
         ],
-        enableIconRotation: true,
-        onTap: (index) {
-          _pageController.animateToPage(
-            index,
-            duration: const Duration(milliseconds: 400),
-            curve: Curves.easeOut,
-          );
-        },
       ),
     );
   }
-}
-
-class homePageScreen extends StatelessWidget {
-  const homePageScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page Screen'),
-    );
-  }
-}
-
-class coursesPage extends StatelessWidget {
-  const coursesPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Courses Page'),
-    );
-  }
-}
-
-class codeEditor extends StatelessWidget {
-  const codeEditor({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Code Editor'),
-    );
-  }
-}
-
-class profilePage extends StatelessWidget {
-  const profilePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
-    );
-  }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: homePage(),
-  ));
 }
