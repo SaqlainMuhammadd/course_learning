3e5214855b8b226040be5ffe38b8c3ce98da4210
muhammadsaqlain143 <miansaqlainqureshi@gmail.com>
2024-06-06 20:01:42 +0500

    javaquiz

---
+++ lib/quiz/java.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class javaQuiz extends StatefulWidget {
+  const javaQuiz({super.key});
+
+  @override
+  State<javaQuiz> createState() => _javaQuizState();
+}
+
+class _javaQuizState extends State<javaQuiz> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
